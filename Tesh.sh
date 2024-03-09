eb099a38eddfcec0fe52d24a97fe18943b7329b3
siddalingaswami11 <siddalingaswami11@gmail.com>
2024-03-09 10:16:30 +0530

    Reverted...Revert "updated file"
    This reverts commit 24884bd22dd1d09a5fc4dc2b9384485a78f804ae.

--- Tesh.sh
+++
@@ -1,4 +0,0 @@
-checking purpose i have updated the test.sh file:.
-
-HI hello,
-This file contains the shellscript file.
--- deployee.sh
+++ deployee.sh
@@ -1,7 +1,3 @@
-
-Just now we are going to updated this file.
-
-
 this file 
 contains the 
 deployement logic. 
